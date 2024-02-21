87a1e062866854599b836ff1db7ece333a382675
Danny Gerst <d.gerst@bizrock.de>
2024-02-21 14:43:07 +0100

    For CivitAI and later for further JSON APIs I have added a request function returing the content directly as JSON

--- loader/utils/scraping_helper.py
+++ loader/utils/scraping_helper.py
@@ -26,6 +26,16 @@ def get_request(*args, **kwargs) -> str:
         return str(e)  # Gibt die Fehlermeldung als String zurück
 
 
+def get_request_as_json(*args, **kwargs) -> str:
+    """Synchronous GET request with requests."""
+    try:
+        response = requests.get(*args, **kwargs)
+        response.raise_for_status()  # Löst eine Exception für HTTP Fehlercodes aus
+        return response.json()
+    except RequestException as e:
+        return str(e)  # Gibt die Fehlermeldung als String zurück
+
+
 def filter_articles(raw_html: str) -> str:
     """Filters HTML out, which is not enclosed by article-tags.
     Beautifulsoup is inaccurate and slow when applied on a larger
